f2b44fed633787685afbe61578556813d4b9c417
manognyaa <77045147+manognyaa@users.noreply.github.com>
2022-07-11 11:29:31 +0530

    Worked on Comments
    Signed-off-by: manognyaa <77045147+manognyaa@users.noreply.github.com>

--- entry/src/main/js/MainAbility/pages/index/index.js
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -13,59 +13,45 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import document from '@ohos.document';
+
 export default {
     data: {
         time_h: "09",
         time_m: "30",
-        time_period:"AM",
+        time_period: "AM",
     },
     onInit() {
         this.fetchTime();
     },
-    fetchTime : function(){
+    fetchTime: function () {
         let date = new Date();
-
-
         let hrs = date.getHours() + ""
 
-        // 4 7
-        if(hrs <10)
-        {
-            this.time_h_0 = 0 ;
+        if (hrs < 10) {
+            this.time_h_0 = 0;
             this.time_h_1 = hrs[0];
         }
-        else
-        {
+        else {
             this.time_h_0 = hrs[0];
             this.time_h_1 = hrs[1];
         }
-
         let min = date.getMinutes() + ""
 
-        if(min <10)
-        {
+        if (min < 10) {
             this.time_m_0 = 0;
             this.time_m_1 = min[0];
         }
-        else
-        {
+        else {
             this.time_m_0 = min[0];
             this.time_m_1 = min[1];
         }
-
-        //        this.time_m_0 = 9
-        //        this.time_m_1 = 9
-
-        this.time_period = date.getHours()  >= 12 ? 'PM' : 'AM';
-
+        this.time_period = date.getHours() >= 12 ? 'PM' : 'AM';
     },
-    change: function(e) {
+    change: function (e) {
         console.log("Tab index: " + e.index);
     },
-    onTap: function(ele)
-    {
-        console.log(this.$element(ele)+" "+this.$element(ele).style.width);
+    onTap: function (ele) {
+        console.log(this.$element(ele) + " " + this.$element(ele).style.width);
         this.$element(ele).style.width = '500fp';
         this.$element(ele).style.height = '50fp';
         console.log(this.$element('myVideo'))
